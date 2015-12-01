a4bd6ad292e32e59985e43caba609c2146d6b787
Dkangni <kangni.devis@yahoo.fr>
2015-12-01 16:02:38 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+html>
+<head>
+    <title>Exercice 2</title>
+	<meta charset="UTF-8">
+</head>
+	<body>
+		<form action="reponse.php" method="POST" enctype="multipart/form-data">
+		    <label for="lastname">Nom</label>
+		    <input type="text" placeholder="Nom" id="lastname" name="lastname" value=""/>
+		    
+		    <label for="firstname">Prénom</label>
+		    <input type="text" placeholder="Prénom" id="firstname" name="firstname" value="" />
+
+		    <label for="age">Age</label>
+		    <input type="number" placeholder="Age" id="age" name="age" value="" />
+
+		    <input type="submit" value="Valider" name="bouton" />
+		</form>
+	</body>
+</html>
